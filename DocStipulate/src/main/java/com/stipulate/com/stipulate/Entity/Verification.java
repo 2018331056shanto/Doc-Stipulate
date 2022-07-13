a9b89d5f6402654d8e758edcf05f8ca557c5ea2d
2018331056shanto <ashraful1185058@gmail.com>
2022-07-13 10:30:49 +0600

    Email verification Completed

---
+++ DocStipulate/src/main/java/com/stipulate/com/stipulate/Entity/Verification.java
@@ -0,0 +1,61 @@
+package com.stipulate.Entity;
+
+
+
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.query.criteria.internal.expression.function.CurrentTimeFunction;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+@Entity
+@Table(name="verification")
+public class Verification {
+	
+	@Id
+	@NotBlank
+	private String email;
+	@Column(name="token")
+	@NotBlank
+	private String token;
+	@CreationTimestamp
+	@Column(name="timestamp")
+	@NotNull
+	private Timestamp createdAt;
+	public Verification() {
+		
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
+	
+
+}
