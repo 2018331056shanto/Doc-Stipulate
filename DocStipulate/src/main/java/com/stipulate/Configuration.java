e422e1bc3053cfa828bc22944d3681da6ab4365c
2018331056shanto <ashraful1185058@gmail.com>
2022-09-26 19:23:09 +0600

    intilij setup

--- DocStipulate/src/main/java/com/stipulate/Configuration.java
+++ DocStipulate/src/main/java/com/stipulate/Configuration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.stipulate.SiteMeshFilter.MySitemeshFilter;
+
 
 @org.springframework.context.annotation.Configuration
 @ComponentScan(basePackages = "com.stipulate")
@@ -13,12 +13,6 @@ import com.stipulate.SiteMeshFilter.MySitemeshFilter;
 @EnableJpaRepositories
 public class Configuration {
 
-//	@Bean
-    FilterRegistrationBean<MySitemeshFilter> siteMeshFilter() {
-        FilterRegistrationBean<MySitemeshFilter> filterRegistrationBean = new FilterRegistrationBean();
-        filterRegistrationBean.setFilter(new MySitemeshFilter()); // adding sitemesh filter ??
-        
-        return filterRegistrationBean;
-    }
+
 
 }
